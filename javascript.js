1ae0d7faf0bf2f3c565d65d40518108e2dc1a2da
Austin T <austin.taranto@gmail.com>
2020-03-27 11:47:16 -0400

    mouse move function update and other function written out

--- javascript.js
+++ javascript.js
@@ -8,9 +8,29 @@ speed.addEventListener("mousemove", function(e) {
     const min = 0.4;
     const max = 4;
     const height = Math.round(percent * 100) + "%";
+    const playbackRate = percent * (max - min) + min;
     bar.style.height = height;
+    bar.textContent = playbackRate.toFixed(2) + "x";
+    video.playbackRate = playbackRate;
+    // console.log(playbackRate);
     // console.log(height);
-    console.log(percent);
+    // console.log(percent);
     // console.log(y);
     // console.log(e);
+
+    // or 
+
+    // function handleMove(e) {
+    //     const y = e.pageY - this.offsetTop;
+    // const percent = y / this.offsetHeight;
+    // const min = 0.4;
+    // const max = 4;
+    // const height = Math.round(percent * 100) + "%";
+    // const playbackRate = percent * (max - min) + min;
+    // bar.style.height = height;
+    // bar.textContent = playbackRate.toFixed(2) + "x";
+    // video.playbackRate = playbackRate;
+    // }
+
+    // speed.addEventListener("mousemove", handleMove);
 });
